44dc0f11829e7f32abf2bb89f7228c2400bb2db8
masterbhuvan1 <bhuvanjain212003@gmail.com>
2023-08-11 15:42:09 +0530

    fifth commit

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ import { getApiConfiguration } from "./store/homeSlice";
 import { fetchDataFromApi } from "./utils/api";
 import { Details } from "./pages/details/Details";
 import { SearchResult } from "./pages/searchResult/SearchResult";
-import { PageNotFound } from "./pages/404/pageNotFound";
+// import { PageNotFound } from "./pages/404/pageNotFound";
 import { Explore } from "./pages/explore/explore";
 import Footer from "./components/footer/Footer";
 import { getGenres } from "./store/homeSlice";
