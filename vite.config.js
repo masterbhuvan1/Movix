7c06226658de50f12e88ecaafc14d00c0bae671f
masterbhuvan1 <bhuvanjain212003@gmail.com>
2023-08-29 00:28:53 +0530

    first commit

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  // base: "/Movix/",
+  base: "/Movix/",
   plugins: [react()],
   server: {
     port: 3002,
